5fbc67acebb4231f523fc3aed60b91f1eeac6527
Gabriel Belo <gabrielbelolima@gmail.com>
2023-09-26 10:36:04 -0300

    creates funcs and login_params

---
+++ login_params.py
@@ -0,0 +1,6 @@
+login_params = {
+                'securityToken' : 'xX0xXxx0X0X0xXxXXXXxXxx0',
+                'email' : 'email@domain.com',
+                'password' : 'user_password',
+                'domain.my': 'domain.my'
+                }
